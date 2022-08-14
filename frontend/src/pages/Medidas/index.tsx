ae538828924dfd07b174e85836dd96d4894ce917
LinikerNogueiraMachado <linikernogueiramachado@gmail.com>
2022-08-14 20:01:39 -0300

    editando index medidas

---
+++ frontend/src/pages/Medidas/index.tsx
@@ -0,0 +1,18 @@
+import './style.css';
+
+//import MedidaBG from 'assets/img/';
+
+function Medidas() {
+
+    return (
+
+        <>
+            <h1>Pagina sob medida</h1>
+            <div>
+                <img src={} alt="Imagem ilustrativa" />
+            </div>
+        </>
+    );
+}
+
+export default Medidas;
